4509cf8395f225cb919372cf0e55af9546830fb5
jkkitti <j.kittiwiriya@gmail.com>
2026-03-19 21:19:09 +0700

    add files

---
+++ js-functions/arrow-functions.js
@@ -0,0 +1,14 @@
+const getData = () => {};
+// arrow function was introduced in ES6, ECMAScript
+
+//anonymous function
+() => {};
+
+//arrow function
+const greetUser = (name) => {
+  return Hello what's is up man ${name};
+};
+
+greetUser();
+
+console.log(greetUser("Aod"));
